7565cdc654d02574db234016f75208d73d8be680
doloiEoh <do.loi@eoh.io>
2024-05-30 21:46:54 +0700

    fix code

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -195,9 +195,7 @@ LOGGING = {
 }
 
 
-CORS_ORIGIN_WHITELIST = [
-    'http://wechatdoloibucket.s3-website-ap-southeast-1.amazonaws.com',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
     'DELETE',
@@ -211,11 +209,4 @@ CORS_ALLOW_METHODS = [
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
 ]
